ee61780f422b9f0a267121eed6c3be72c2dd39b1
Cristian Lenin Roblero <cristianroblero00@gmail.com>
2025-12-01 06:07:11 -0500

    Importacion de plantilla

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function login(Request $request)
+    {
+        /*
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            $usuario = Auth::user();
+
+            if ($usuario->role === 'admin') {
+                return redirect('/admin/inicio');
+            } elseif ($usuario->role === 'visitante') {
+                return redirect('/usuariosVista/usuarioRiesgo');
+            }
+
+            Auth::logout();
+            return back()->withErrors(['email' => 'Rol no permitido.']);
+        }
+
+        return back()->withErrors(['email' => 'Credenciales incorrectas.']);*/
+        return redirect('/admin/inicio');
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PaisController;
+use App\Http\Controllers\UserController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login.iniciarSesion');
 });
 
 Route::resource('pais',PaisController::class);
+Route::get('/admin/inicio', function () {
+    return view('layout.admin');
+});
+
+
+Route::post('/login', [UserController::class, 'login'])->name('users.login');
